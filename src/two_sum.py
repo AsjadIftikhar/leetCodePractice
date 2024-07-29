15908ea7af80ad8e067874c6b426ffee7f85dd9e
Asjad <iftikharasjad@gmail.com>
2024-07-29 14:53:49 +0500

    optimized solution

--- src/two_sum.py
+++ src/two_sum.py
@@ -9,10 +9,11 @@ from typing import List
 
 
 def twoSum(nums: List[int], target: int) -> List[int]:
+    storage = {}
     for i in range(len(nums)):
-        for j in range(i + 1, len(nums)):
-            if nums[i] + nums[j] == target:
-                return [i, j]
+        if target - nums[i] in storage:
+            return [i, storage[target - nums[i]]]
+        storage[nums[i]] = i
 
 
 nums = [3, 2, 4]
