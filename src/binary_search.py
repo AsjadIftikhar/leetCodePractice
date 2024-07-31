176e3af592501370f7b499d389cd0c98675b8f8e
Asjad <iftikharasjad@gmail.com>
2024-07-31 19:16:14 +0500

    binary search

---
+++ src/binary_search.py
@@ -0,0 +1,23 @@
+from typing import List
+
+
+def search(nums: List[int], target: int) -> int:
+    left, right = 0, len(nums) - 1
+
+    while left <= right:
+        mid = left + (right - left) // 2
+
+        if nums[mid] == target:
+            return mid
+        elif nums[mid] < target:
+            left = mid + 1
+        else:
+            right = mid - 1
+
+    return -1
+
+
+nums = [5]
+target = 5
+
+print(search(nums, target))
