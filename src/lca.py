73387b3914778562d34f74960d13a5858dfc85a7
Asjad <iftikharasjad@gmail.com>
2024-08-02 12:19:03 +0500

    lowest common ancestor

---
+++ src/lca.py
@@ -0,0 +1,18 @@
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+def lowestCommonAncestor(root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+    if root is None:
+        return None
+    if p.val < root.val and q.val < root.val:
+        return lowestCommonAncestor(root.left, p, q)
+    if p.val > root.val and q.val > root.val:
+        return lowestCommonAncestor(root.right, p, q)
+
+    return root
+
+
